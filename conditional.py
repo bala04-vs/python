57dfa5aa59b052373907991e6e0447b60f82090d
bala04-vs <venkatposina29@gmail.com>
2025-03-21 15:09:36 +0530

    add

---
+++ conditional.py
@@ -0,0 +1,41 @@
+# 2
+
+
+ #  check given year is leap year or not
+Year=int(input("enter the year"))
+if(Year%4==0):
+    print("given year is leap year")
+else:
+    print("is not leap year")  
+
+
+# check given character is vowel are consonant
+
+Character=str(input("enter the string"))
+Vowels=["a","e","i","o","u"]
+consonants = [
+  'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm',  
+  'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'
+]
+if(Character==Vowels):
+    print("given character is Vowel")
+elif (Character==consonants):
+    print("given character is consonant")
+else:
+    print("not a alaphabet") 
+
+    
+            
+
+
+
+
+
+
+
+
+
+
+                  
+        
+
